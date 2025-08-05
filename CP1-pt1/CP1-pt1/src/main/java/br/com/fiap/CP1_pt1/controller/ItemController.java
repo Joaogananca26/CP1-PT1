4f5df6ed862cd9578f531570b32a830279c56230
viniciuslabernardes <labsfiap@fiap.com.br>
2025-08-05 20:55:40 -0300

    começando controller

--- CP1-pt1/CP1-pt1/src/main/java/br/com/fiap/CP1_pt1/controller/ItemController.java
+++ CP1-pt1/CP1-pt1/src/main/java/br/com/fiap/CP1_pt1/controller/ItemController.java
@@ -3,6 +3,10 @@ package br.com.fiap.CP1_pt1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
+@RequestMapping("/ferramentas")
 public class ItemController {
+
+
+
 }
